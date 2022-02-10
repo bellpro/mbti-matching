3a810cf8f5cc3839ef850cba16bf8967f64f9b8b
daily220416 <daily190920@gmail.com>
2022-02-11 06:53:36 +0900

    Initial commit

---
+++ src/test/java/com/mbti/mbtimatching/MbtiMatchingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbti.mbtimatching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbtiMatchingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
